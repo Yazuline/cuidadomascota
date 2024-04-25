8c2598ed6f9ae220877da488f5d1b2e6cdccc175
Yazuline Viveros Bedoya <ingeniera-yazuline@hotmail.com>
2024-04-24 22:57:49 -0500

    cambio url config frontend

--- mascotapp/frontend/src/services/config.ts
+++ mascotapp/frontend/src/services/config.ts
@@ -1,6 +1,6 @@
 // config.ts
 
 const PORT = process.env.PORT || 3001;
-const API_URL = `https://cuidadomascota.vercel.app:${PORT}/`  
+const API_URL = `https://cuidadomascota.vercel.app:${PORT}`  
 export default API_URL;
 
