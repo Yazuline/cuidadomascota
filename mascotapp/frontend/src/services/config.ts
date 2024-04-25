127d3912b9a64a58d03880be6a3f35628257cede
Yazuline Viveros Bedoya <ingeniera-yazuline@hotmail.com>
2024-04-25 02:32:22 -0500

    api add

--- mascotapp/frontend/src/services/config.ts
+++ mascotapp/frontend/src/services/config.ts
@@ -1,6 +1,6 @@
 // config.ts
 const PORT = process.env.PORT || 3001;
-const API_URL = `https://cuidadomascota-jvx5-server.vercel.app/`  
+const API_URL = `https://cuidadomascota-jvx5-server.vercel.app`  
 
 export default API_URL;
 
